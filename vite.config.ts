94875d6c2608dd0fcf996e1fa2464ff7143584a5
renat <murzinarenata@gmail.com>
2024-06-14 21:32:04 -0700

    netlify.toml was changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    mkcert()
-  ],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    https: true,
-    proxy: {
-      '/': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/$/, '/index.html'),
-      },
-    },
-  }
+  plugins: [react()],
 })
